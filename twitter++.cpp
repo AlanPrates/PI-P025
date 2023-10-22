78948b04af06eca0c19ba8b1a161b571e3dfd03c
paulo2k21 <paulomarques.ufsb@gmail.com>
2023-10-22 11:01:37 -0300

    Construindo twitter++ ver 0.1

---
+++ twitter++.cpp
@@ -0,0 +1,18 @@
+/*
+Exercício 2: Desenvolvimento de uma Rede Social Baseada no Twitter 
+ 
+Você foi contratado para desenvolver uma rede social baseada no Twitter (agora X 
+           ). A rede social terá funcionalidades básicas, como a capacidade de criar perfis de 
+usuário, postar tweets (apenas texto até 255 caracteres), seguir outros usuários e 
+receber feeds de tweets de pessoas que você segue. 
+ 
+Funcionalidades Principais: 
+• Os usuários podem se registrar na rede social, especificando um nome de 
+usuário único e seu nome real. 
+• Os usuários podem postar tweets. 
+• Os usuários podem seguir outros usuários. 
+• Os usuários podem ver um feed de tweets dos usuários que estão seguindo. 
+• Os tweets devem ser exibidos em ordem cronológica, com os mais recentes 
+no topo. 
+• Os dados devem ser salvos e recuperados em arquivo. 
+*/
